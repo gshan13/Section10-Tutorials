27ec4bda24df95074770d5abbce82739452a2c6e
Gary Shan <gshan@academic.rrc.ca>
2024-02-01 13:05:32 -0600

    241
    Video 241

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,3 +1,5 @@
+import {MongoClient} from 'mongodb';
+
 function handler(req, res) {
     if (req.method === 'POST') {
         const userEmail = req.body.email;
@@ -6,6 +8,8 @@ function handler(req, res) {
             res.status(422).json({message: 'Invalid email address.'});
             return;
         }
+
+        MongoClient.
 
         console.log(userEmail);
         res.status(201).json({message: 'Signed up!'});
